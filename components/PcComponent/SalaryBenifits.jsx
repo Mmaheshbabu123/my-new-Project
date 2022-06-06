1a4e79dd68a7ac01b66a1ad8afc58ee4965928a9
supreetha <supreetha.s@infanion.com>
2022-06-06 15:08:40 +0530

    eslint erroe

--- components/PcComponent/SalaryBenifits.jsx
+++ components/PcComponent/SalaryBenifits.jsx
@@ -5,15 +5,15 @@ const SalaryBenifits = () => {
 
 	return (
 		<div className="container">
-			<div class="form-check mt-4">
+			<div calssName="form-check mt-4">
 				<input
-					class="form-check-input"
+					calssName="form-check-input"
 					type="checkbox"
 					value=""
 					id="flexCheckDefault"
 					onClick={(e) => setVisible(!visible)}
 				/>
-				<label class="form-check-label p-1" for="flexCheckDefault">
+				<label calssName="form-check-label p-1" htmlFor="flexCheckDefault">
 					Salary (remuneration) - compensatory rest
 				</label>
 			</div>
@@ -24,14 +24,14 @@ const SalaryBenifits = () => {
 						<div className="d-flex mt-3">
 							<div className="form-check  ">
 								<input className="form-check-input d-flex" type="radio" value="option1" />
-								<label className="form-check-label ms-1" for="exampleRadios1">
+								<label className="form-check-label ms-1" htmlFor="exampleRadios1">
 									Yes
 								</label>
 							</div>
 
 							<div className="form-check">
 								<input className="form-check-input ms-2" type="radio" value="option2" />
-								<label className="form-check-label ms-1" for="exampleRadios2">
+								<label className="form-check-label ms-1" htmlFor="exampleRadios2">
 									No
 								</label>
 							</div>
