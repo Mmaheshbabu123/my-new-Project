a4269f97ff7b1a1a06b093550c110e0f5f93d29d
preeti <preeti.k@infanion.com>
2022-06-07 19:49:42 +0530

    1940553: planning page alterations

--- Services/ApiEndPoints.js
+++ Services/ApiEndPoints.js
@@ -17,8 +17,12 @@ export const getPc = BASE_URL + "getParitaireCommitteFunctionCategory";
 export const addPc = BASE_URL + "addpc"
 
 //----- EMPLOYEE TYPE and COEFFICIENT API ROUTES -----//
-export const fetchEmployeeTypes  =  BASE_URL + "get-employee-types";
-export const createEmployeeTypes =  BASE_URL + "create-employee-types";
-export const editEmployeeType    =  BASE_URL + "edit-employee-type";
-export const deleteEmployeeType    =  BASE_URL + "delete-employee-type";
+export const fetchEmployeeTypes = BASE_URL + "get-employee-types";
+export const createEmployeeTypes = BASE_URL + "create-employee-types";
+export const editEmployeeType = BASE_URL + "edit-employee-type";
+export const deleteEmployeeType = BASE_URL + "delete-employee-type";
 //-----
+
+
+//--------------PLANNING API ROUTES ----------------//
+export const addPlanning = BASE_URL + "addplanning";
