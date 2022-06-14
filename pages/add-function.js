52eca9e020ec16f26b1a31dfc09af57bbfd97365
supreetha <supreetha.s@infanion.com>
2022-06-14 22:44:00 +0530

    package-json

--- pages/add-function.js
+++
@@ -1,7 +0,0 @@
-import { Layout } from "../components/layout/Layout";
-import AddFunction from "../components/PcComponent/AddFunction";
-
-const Addfunction = () => (
-    <div><AddFunction /></div>
-)
-export default Addfunction;
