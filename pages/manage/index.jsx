da0c8be9a6fc737301c51a34bb00093725c19f9e
mahesh <maheshbabumanikanti.infanion@gmail.com>
2022-06-20 15:51:54 +0530

    Mahesh_naming_changes_200621

--- pages/manage/index.jsx
+++ pages/manage/index.jsx
@@ -6,28 +6,28 @@ function ManageEmployeeAndCoefficientTypes() {
       <Link href="/manage/employee-types">
         <a className='show-box-link'>
           <div title="Manage employee types">
-            Employee types
+          Manage employee types
           </div>
         </a>
       </Link>
       <Link href="/manage/coefficient-types">
         <a className='show-box-link'>
           <div title="Manage coefficient types">
-            Coefficient types
+          Manage coefficient types
           </div>
         </a>
       </Link>
       <Link href="/link-coefficient-employeetype?pcid=0">
         <a className='show-box-link'>
           <div title="Link coefficients to employee types">
-            Link coefficients to employee type
+          Link coefficients to employee types
           </div>
         </a>
       </Link>
       <Link href="/linkcofficientpc/manage">
         <a className='show-box-link'>
           <div title="Manage coefficient per PC">
-            Manage coefficients per pc
+            Manage coefficient per PC
           </div>
         </a>
       </Link>
