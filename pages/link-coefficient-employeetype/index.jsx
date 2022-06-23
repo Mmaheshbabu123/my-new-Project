d4303e0fa7987aebf466c63b59b9b368d89c9c9c
sachin <sachinm.infanion@gmail.com>
2022-06-23 22:51:23 +0530

    TODOID:: code added related adding pagination for all pages (employee types, coefficients, salary benefits and manage pc per coefficients)

--- pages/link-coefficient-employeetype/index.jsx
+++ pages/link-coefficient-employeetype/index.jsx
@@ -1,7 +1,7 @@
 import React, { Suspense } from 'react';
 import { useRouter } from 'next/router';
 import dynamic from 'next/dynamic'
-const LinkCoeffEmpStates = dynamic(() => import('../../Contexts/LinkCoeffEmp/LinkCoeffEmpStates'), { ssr: false });
+import LinkCoeffEmpStates from '../../Contexts/LinkCoeffEmp/LinkCoeffEmpStates';
 const LinkCoeffEmpComponent = dynamic(() => import('../../components/LinkCoeffEmpComponents/LinkCoeffEmpComponent'), { ssr: false, suspense: true });
 
 const LinkCoefficientEmployeetype = () => {
