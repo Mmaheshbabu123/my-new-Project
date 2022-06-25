7888e07528d73303f217ebdbe5c71eca42df04bc
sachin <sachinm.infanion@gmail.com>
2022-06-25 11:34:16 +0530

    added coefficient per pc tile icon

--- components/tileIcon/tileJson.js
+++ components/tileIcon/tileJson.js
@@ -2,6 +2,7 @@ import employeetype from '@/components/images/manage-employee-type@2x.png';
 import coefficient from '@/components/images/manage-co-efficients@2x.png';
 import linkcofficientemp from '@/components/images/link-coefficients_to_employee-types@2x.png';
 import salary from '@/components/images/salary-benefits@2x.png';
+import coefficientperPC from '@/components/images/manage_coefficient_per_pc@2x.png';
 
 export const tileJson = [
   {
@@ -26,7 +27,7 @@ export const tileJson = [
     id: 4,
     name: 'Manage coefficient per PC',
     url: '/linkcofficientpc/manage',
-    iconPath: coefficient,
+    iconPath: coefficientperPC,
   },
   {
     id: 5,
