dba1f75905e57503bcbf0364730e4cb883a3234d
Sathish <sathish.kumar@infanion.com>
2022-06-27 01:52:18 +0530

    "planning flow_jun27"

--- components/PlanningComponent/addFunction.jsx
+++ components/PlanningComponent/addFunction.jsx
@@ -6,65 +6,102 @@ import ValidationService from '../../Services/ValidationService';
 import { useRouter } from 'next/router';
 import { Printer } from 'react-bootstrap-icons';
 
-
 const AddFunction = () => {
-	const companyid=4567;
-	const pc=112233;
+	const companyid = 4567;
+	const pc = 112233;
 	const router = useRouter();
 	const [ Data, setData ] = useState([]);
+	const [ emptypes, setEmptypes ] = useState([]);
 
 	useEffect(() => {
-		APICALL.service("http://absoluteyou-backend.local/api/get-planningemployee/"+companyid, 'GET')
-			.then(async(result) => {
-				var data=result.data;
-				var employees=[];
-				
+		APICALL.service(
+			process.env.NEXT_PUBLIC_APP_BACKEND_URL + 'api/getemployeetypebypcnumber/4567,112233',
+			'GET'
+		).then(async(respons) => {	
+				console.log(respons);
+			})
+			.catch((error) => {
+				console.error(error);
+			});
+
+		APICALL.service( process.env.NEXT_PUBLIC_APP_BACKEND_URL+'api/get-planningemployee/' + companyid, 'GET')
+			.then(async (result) => {
+				var data = result.data;
+				var employees = [];
 				for (var i = 0; i < data.length; i++) {
-					await APICALL.service(process.env.NEXT_PUBLIC_APP_URL_DRUPAL+"getemployeebycompany/?_format=json&emp_id="+data[i][3], 'GET')
-					.then((res) => {
-						console.log(res);
-				      employees.push([res[0]['Employee_id'],res[0]['Employee_name']]);
-					})
-					.catch((error) => {
-						console.error(error);
-					});
+					await APICALL.service(
+						process.env.NEXT_PUBLIC_APP_URL_DRUPAL +
+							'getemployeebycompany/?_format=json&emp_id=' +
+							data[i][3],
+						'GET'
+					)
+						.then((res) => {
+							employees.push([ res[0]['Employee_id'], res[0]['Employee_name'] ]);
+						})
+						.catch((error) => {
+							console.error(error);
+						});
 				}
 				await setData(employees);
-				console.log(Data);
+			})
+			.catch((error) => {
+				console.error(error);
+			});
+		//fetching employee types name,id
+		var employeetypes=[];
+		APICALL.service(
+			process.env.NEXT_PUBLIC_APP_BACKEND_URL + 'api/getemployeetypebypcnumber/' + pc,
+			'POST'
+		)
+			.then(async(res) => {	
+				var result=res.employeetypes;
+				for(let i=0;i<result.length;i++){
+					var opt = {
+						value: '',
+						label: ''
+					};
+					opt.value = result[i]['id'];
+					opt.label = result[i]['name'];
+					employeetypes[i] = opt;
+				}
+				await setEmptypes(employeetypes);
 			})
 			.catch((error) => {
 				console.error(error);
 			});
 	}, []);
 
-	
-	const submit=(e)=>{
-
+	const submit = (e) => {
 		e.preventDefault();
-		
-		}
-	
+	};
 
 	return (
-		<div className="container" style={{ marginTop: '15%', marginBottom: '2%' }}>
+		<div className="container" style={{ marginTop: '5%', marginBottom: '2%' }}>
 			<form onSubmit={(e) => submit(e)}>
 				<div className="row">
-					<div className="row" >
-						<h1 style={{ display: 'inherit', fontSize: '30px' , fontWeight: 'bold' }}>Add function</h1>
-					</div>
 					<div className="row">
-					<div><input type="checkbox" id="sameforall" name="sameforall" value="sameforall"/>
-					<label>Same function for all employees</label></div>
+						<h1 style={{ display: 'inherit', fontSize: '30px', fontWeight: 'bold' }}>Add function</h1>
+					</div>
+					<div className="row" style={{marginBottom:'1%'}}>
+						<div>
+							<input type="checkbox" id="sameforall" name="sameforall" value="sameforall" />
+							<label style={{paddingLeft:'10px'}}>Same function for all employees</label>
+						</div>
 					</div>
 				</div>
-				<div className="row">
+				<div className="row" >
 					<ol type="1">
-					{
-						Data.map((key,value)=>
-						     
-							<div>{value+1}. { key[1] }<Select options={Data} name='employees' /> </div>
-						)	
-					}
+						{Data.map((key, value) => (
+							<div className='row' style={{marginBottom: "1%",backgroundColor:'gray'}}>
+							<div className='col-md-3'>
+								{value + 1}. {key[1]}
+							</div>
+                            <div className='col-md-4'>
+								<Select options={emptypes} name="employees" />
+							</div>
+							</div>
+							
+						))}
 					</ol>
 				</div>
 				<div className="row">
@@ -81,7 +118,7 @@ const AddFunction = () => {
 						<button
 							type="sumit"
 							className="btn btn-secondary btn-lg btn-block float-sm-right mt-5 md-5 add-proj-btn"
-						    onClick={()=>submit} 
+							onClick={() => submit}
 						>
 							Next
 						</button>
