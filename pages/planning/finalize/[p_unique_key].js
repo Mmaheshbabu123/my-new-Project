bcdb1b7b8caa4270a4918660db742e073a253824
supreetha <supreetha.s@infanion.com>
2022-06-27 15:11:57 +0530

    planning

---
+++ pages/planning/finalize/[p_unique_key].js
@@ -0,0 +1,11 @@
+import PlanningFinalize from "../../../components/PlanningComponent/PlanningFinalize";
+
+const finalizeplanning = () => {
+
+return(<div>
+    <PlanningFinalize/>
+</div>);
+
+}
+
+export default finalizeplanning;
