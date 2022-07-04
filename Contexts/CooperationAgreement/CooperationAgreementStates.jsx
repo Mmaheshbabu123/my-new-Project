252f5b9aff1c62872d775a184d4ce8cc04e1079e
mahesh <maheshbabumanikanti.infanion@gmail.com>
2022-07-04 10:39:02 +0530

    Mahesh_default_settings_tab6

--- Contexts/CooperationAgreement/CooperationAgreementStates.jsx
+++ Contexts/CooperationAgreement/CooperationAgreementStates.jsx
@@ -10,7 +10,7 @@ const CooperationAgreementStates = (props) => {
     , tab_3 : {loaded:false}
     , tab_4 : {'43':1,'44':2,'48':1,'49':1}
     , tab_5 : {}
-    , tab_6:  {}
+    , tab_6:  {'56':1,'57':2,'58':1,'61':2,'62':2,'63':2,'64':2,'65':2,'73':2,'74':1,'76':2,'77':2,'78':2,'79':2}
     , selectedTabId: 1
     , loadedTabs: []
     , renderTabComponents: false
