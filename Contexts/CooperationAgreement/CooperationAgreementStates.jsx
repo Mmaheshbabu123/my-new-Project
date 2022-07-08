34bf5b616174c9e20a02b27dd87114b51654e535
mahesh <maheshbabumanikanti.infanion@gmail.com>
2022-07-08 12:13:30 +0530

    Mahesh_postalcode_validation

--- Contexts/CooperationAgreement/CooperationAgreementStates.jsx
+++ Contexts/CooperationAgreement/CooperationAgreementStates.jsx
@@ -23,7 +23,7 @@ const takeElementStatus = (type = 1) => type === 1 ?
 const CooperationAgreementStates = (props) => {
   const initialState = {
       tab_1 : { worksServantsData: {1: [], 2: []}, cooperationCoeffData: {} }
-    , tab_2 : {'22':1,'23':1,'24':1,validations:{'17':{'type':1,validate:false,'text':'Only numbers will accept' },'19':{'type':2,validate:false},'14':{'type':1,validate:false,'text':'Only numbers will accept'},'18':{'type':1,validate:false,'text':'Only numbers will accept'}}}
+    , tab_2 : {'22':1,'23':1,'24':1,validations:{'17':{'type':1,validate:false,'text':'Only numbers will accept' },'19':{'type':2,validate:false},'14':{'type':3,validate:false,'text':'Only numbers will accept'},'18':{'type':1,validate:false,'text':'Only numbers will accept'}}}
     , tab_3 : {1:{'25':1,'30':1,'31':2,'32':2,'38':2,'39':2,validations:{'34':{'type':1,validate:false},'35':{'type':2,validate:false}}},2:{'25':1,'30':1,'31':2,'32':2,'38':2,'39':2,validations:{'34':{'type':1,validate:false},'35':{'type':2,validate:false}}},loaded:true,}
     , tab_4 : {'43':1,'44':2,'48':1,'49':1,validations:{'40':{'type':2,validate:false},'41':{'type':2,validate:false},'42':{'type':2,validate:false},'45':{'type':2,validate:false},'46':{'type':2,validate:false},'47':{'type':2,validate:false}}}
     , tab_5 : {
@@ -32,7 +32,7 @@ const CooperationAgreementStates = (props) => {
       cooperationBenefits: {},
     }
     , tab_6:  {'56':1,'57':2,'58':1,'61':2,'62':2,'63':2,'64':2,'65':2,'73':2,'74':1,'76':2,'77':2,'78':2,'79':2,
-       validations:{'54':{'type':1,validate:false},'55':{'type':2,validate:false} }
+       validations:{'54':{'type':1,validate:false},'55':{'type':2,validate:false} ,'52':{'type':3,validate:false},}
      }
     , selectedTabId: 1
     , loadedTabs: []
