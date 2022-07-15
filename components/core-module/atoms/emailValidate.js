3e7e614e4b4f742c1aad858464eb0ab3e2d2c92e
mahesh <maheshbabumanikanti.infanion@gmail.com>
2022-07-15 15:18:06 +0530

    Mahesh_cop_3_issues

--- components/core-module/atoms/emailValidate.js
+++ components/core-module/atoms/emailValidate.js
@@ -1,6 +1,7 @@
 export default function  emailValidate(text) {
+
   var mailformat = new RegExp(/[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,15}/g);
-  if (mailformat.test(text)) {
+  if (mailformat.test(text.toLowerCase())) {
     return true;
   }
   else {
