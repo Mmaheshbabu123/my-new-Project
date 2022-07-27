ffd322961715d05894f5d3807523fa69c239e908
sachin <sachinm.infanion@gmail.com>
2022-07-27 11:27:27 +0530

    code added related to signature flow changes

--- components/SignatureFlow/molecules/SignatureDetails.jsx
+++ components/SignatureFlow/molecules/SignatureDetails.jsx
@@ -1,12 +1,23 @@
 import React from 'react';
 import Modal from 'react-bootstrap/Modal';
 import SignatureField from '@/atoms/SignatureField';
+import { confirmAlert } from 'react-confirm-alert';
 
-const SignatureDetails = ({ state, setState, submitSignData }) => {
+const SignatureDetails = ({ state, setState, submitSignData, eraseSignature }) => {
   const handleSave      = (e) => submitSignData(e);
   const handleClose     = ( ) => setState({...state, showPopup: false})
-  const handleView      = ( ) => setState({...state, showPopup: true, disabled: true })
   const handleEdit      = ( ) => setState({...state, showPopup: true, disabled: false })
+
+  const handleDelete = () => {
+    confirmAlert({
+      message: 'Do you want to delete signature?',
+      buttons: [
+        { label: 'No' },
+        { label: 'Yes', onClick: () => eraseSignature() }
+      ]
+    });
+  }
+
   return(
     <div>
         <Modal  show={state.showPopup} onHide={handleClose}>
@@ -27,12 +38,12 @@ const SignatureDetails = ({ state, setState, submitSignData }) => {
           <span> Sign: </span>
           <div className='col-md-6'> <img src={state.sign} alt="no sign"/> </div>
           <div className='col-md-6'>
-            <button className={`btn btn-light`} style={{width:'30%'}} onClick={handleView}> View </button>
+            <button className={`btn btn-light`} style={{width:'30%'}} onClick={handleDelete}> Delete </button>
             <button className={`btn btn-secondary mx-2`} style={{width:'30%'}} onClick={handleEdit}> Edit </button>
           </div>
           </> :
           <>
-          <p style={{margin: '20px 0', textAlign: 'center'}}> {`We have notice that you haven't signed, please "Add your signature"`} </p>
+          <p style={{margin: '20px 0', textAlign: 'center'}}> {`You haven't added a signature yet, please add your signature.`} </p>
               <button onClick={handleEdit} type="button" className="btn btn-dark pcp_btn col-2 m-auto">
                 {`Add signature`}
               </button>
