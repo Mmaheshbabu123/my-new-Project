ce51c264330756d9f200adff281996ae341f9643
sachin <sachinm.infanion@gmail.com>
2022-08-03 11:28:35 +0530

    code added related to redirection after 6th tab submot

--- components/CooperationAgreementComponents/Tabs/submitService.jsx
+++ components/CooperationAgreementComponents/Tabs/submitService.jsx
@@ -376,7 +376,7 @@ async function forWardToNextStepTab(router, contextState, contextUpdate, current
         }
         if(selectedTabId === INVOIING_TAB && draft !== 1) {
           window.open(`/cooperation-agreement-preview?root_parent_id=${stateObj.root_parent_id}&sales_ref=${stateObj.salesAgentRefId}&type=1`, '_blank');
-          router.push('/manage-cooperation-overview?type=sales_agent&id=1')
+          router.push(`/manage-cooperation-overview?type=sales_agent&id=${stateObj.defaultOptions.agent_details['sales_agent_id']}`);
         } else {
           router.query.selectedTabId = nextTab;
           router.push(router, undefined, { shallow: true })
