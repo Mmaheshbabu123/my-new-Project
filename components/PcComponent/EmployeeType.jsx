4dd0eb6135eff86042b9e9cd7884372eb9c7124d
sreenu <sreenivasulu.perla@infanion.com>
2022-08-09 15:30:50 +0530

    fixed for bugs

--- components/PcComponent/EmployeeType.jsx
+++ components/PcComponent/EmployeeType.jsx
@@ -164,7 +164,7 @@ const EmployeeType = () => {
 			<form onSubmit={submit}>
 			{pc_view_type == 'editpc' ? <h4 className="h5 mt-3">Edit employee type</h4> : (pc_view_type == 'viewpc'?<h4 className="h5 mt-3">Employee type</h4> :'')}
             <div className='pc-height'>
-				<div className="row pt-4 border-form-sec m-0 p-4 ">
+				<div className="row pt-0 border-form-sec m-0 p-4 ">
 				<p className="mt-2 p-0" style={{ color: 'red' }}>
 						{error_emp_type}
 					</p>
