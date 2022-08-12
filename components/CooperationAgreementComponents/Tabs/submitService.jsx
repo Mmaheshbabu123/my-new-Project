cc96fb15f6012684fc916964f9501cc67423b751
sachin <sachinm.infanion@gmail.com>
2022-08-12 15:08:42 +0530

    code added related to save as draft changes

--- components/CooperationAgreementComponents/Tabs/submitService.jsx
+++ components/CooperationAgreementComponents/Tabs/submitService.jsx
@@ -377,7 +377,7 @@ async function forWardToNextStepTab(router, contextState, contextUpdate, current
           router.query.root_parent_id = obj['root_parent_id'];
         }
         if(draft === 1) {
-          setTimeout(() => window.close(), 1000);
+          window.close();
         }
         if(selectedTabId === INVOIING_TAB && draft !== 1) {
           window.open(`/cooperation-agreement-preview?root_parent_id=${stateObj.root_parent_id}&sales_ref=${stateObj.salesAgentRefId}&type=1`, '_blank');
