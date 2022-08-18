8146ea5e35dd18a092164c3f24f4f5d815c113f4
sachin <sachinm.infanion@gmail.com>
2022-08-18 15:11:16 +0530

    code added related react-select search option

--- components/core-module/atoms/MultiSelectField.js
+++ components/core-module/atoms/MultiSelectField.js
@@ -14,15 +14,27 @@ import Select from 'react-select';
     color:'black',
   }),
 }
-export default function MultiSelect({ handleChange, standards, options, disabled, isMulti = false, name = '' ,placeholder = 'Select', className="pc-single-select",customStyle = { }, classNamePrefix = '',
-isSearchable = true
-}) {
+export default function MultiSelect(
+  {
+    handleChange,
+    standards,
+    options,
+    disabled,
+    isMulti = false,
+    name = '',
+    placeholder = 'Select',
+    className="pc-single-select",
+    customStyle = { },
+    classNamePrefix = '',
+    isSearchable = true
+  }
+) {
   const customFilter = (option, searchText) => {
-      if (option.label !== undefined && option.label !== null && option.label.toLowerCase().includes(searchText.toLowerCase())){
+    if (option.label !== undefined && option.label !== null && option.label.toLowerCase().includes(searchText.toLowerCase())){
         return true
-      }
-        return false;
     }
+    return false;
+  }
     return (
       <div className= {className || "col-md-3 mt-2 mb-3 p-0"}>
         <Select
