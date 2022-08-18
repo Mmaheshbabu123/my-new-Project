ee60a28062b3fc41f131229fec9648c446908e84
supreetha <supreetha.s@infanion.com>
2022-08-18 12:54:05 +0530

    pagination

---
+++ components/PcComponent/Pagination.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import ReactPaginate from "react-paginate";
+import { AiOutlineArrowLeft, AiOutlineArrowRight } from 'react-icons/ai';
+
+const Pagination = (props) =>{
+return (
+    <div>
+        <ReactPaginate
+							breakLabel="..."
+							nextLabel={props.itemOffset>0?<AiOutlineArrowRight />:<AiOutlineArrowRight />}
+							onPageChange={props.handlePageClick}
+							pageRangeDisplayed={5}
+							pageCount={props.pageCount}
+							previousLabel={props.itemOffset>0?<AiOutlineArrowLeft />:<AiOutlineArrowLeft />}
+							renderOnZeroPageCount={null}
+							containerClassName={'pagination justify-content-center project-pagination'}
+							itemClass="page-item"
+							linkClass="page-link"
+							subContainerClassName={'pages pagination'}
+							activeClassName={'active'}
+						/>
+    </div>
+)
+
+
+
+}
+
+export default Pagination;
