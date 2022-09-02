b9dd9d82f87afbe1f2eb3fcab825fd2f2014632d
sachin <sachinm.infanion@gmail.com>
2022-09-02 11:03:01 +0530

    code added related to sign as employer flow

--- components/AbsAdminSv/molecules/SalesAgentPopUpComponent.jsx
+++ components/AbsAdminSv/molecules/SalesAgentPopUpComponent.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useRouter } from 'next/router';
 import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
