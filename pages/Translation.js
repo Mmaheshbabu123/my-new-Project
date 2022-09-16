6844ab61ce404f9a64de9c141968017620912010
jagadish <jagadish.c@infanion.com>
2022-09-16 16:35:54 +0530

    2075743:absy_jagadish_translation4_16092022_v1

--- pages/Translation.js
+++ pages/Translation.js
@@ -3,31 +3,49 @@ import { APICALL } from '../Services/ApiServices';
 import { useRouter } from 'next/router';
 
 const t = (input) =>{
-  return input;
+	console.log('input', input);
+    const ISSERVER = typeof window === "undefined";
+    if(!ISSERVER) {
+      let lang = localStorage['servername_'+'lang'] !== undefined ? localStorage['servername_'+'lang'] : 'en'; 
+      let translations = localStorage['servername_'+'translations'] !== undefined ? JSON.parse(localStorage['servername_'+'translations']) : {};
+      if(translations[lang] !== undefined){
+       return translations[lang][input] !== undefined ? translations[lang][input] : input;
+      }else{
+       return input;
+      }
+    }else{
+      return input;
+    }
 }
-const Translation = (Component, stringList, lang) => {
+
+const Translation = (Component, stringList) => {
     const TranslatedComponent = () => {
         useEffect(() => {
          const getTranslationData = async () => {
          let url = process.env.NEXT_PUBLIC_APP_BACKEND_URL + 'api/translate';
+         let lang = localStorage['servername_'+'lang'] !== undefined ? localStorage['servername_'+'lang'] : 'en'; 
           APICALL.service(url, 'POST', {lang:lang, stringList: stringList})
             .then((result) => {
               if(result['status'] == 200){
-                if(localStorage.translations === undefined){
-                  localStorage.setItem('translations', '{}');
+                if(localStorage['servername_'+'translations'] === undefined){
+                  localStorage.setItem('servername_'+'translations', '{}');
                 }
-                let translations = JSON.parse(localStorage.translations);
+                let translations = JSON.parse(localStorage['servername_'+'translations']);
                 if(translations[lang] !== undefined){
                   translations[lang] = {...translations[lang], ...result['data']};
                 }else{
                   translations[lang] = result['data'];
                 }
-                localStorage.setItem('translations', JSON.stringify(translations));
+                localStorage.setItem('servername_'+'translations', JSON.stringify(translations));
               }
             }) 
 	      };
         if(stringList.length > 0){
-          getTranslationData();
+          const ISSERVER = typeof window === "undefined";
+		    if(!ISSERVER) {
+		getTranslationData();
+		    
+		    }
         }
         }, []);
         return <Component t={t}/> 
