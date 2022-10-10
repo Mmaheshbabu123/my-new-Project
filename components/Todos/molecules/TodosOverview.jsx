307481ee8efc70811c92f85a72eb4db2dbc436af
mahesh <maheshbabumanikanti.infanion@gmail.com>
2022-10-10 15:37:59 +0530

    Mahesh_wpf_101022

--- components/Todos/molecules/TodosOverview.jsx
+++ components/Todos/molecules/TodosOverview.jsx
@@ -123,7 +123,7 @@ const TodosOverview = ({ props, entityId, entityType, tabId }) => {
     return <>
       {eachRow.todo_type === 2 ?
         <>
-          {eachRow.todo_status !== 1 && <span title={'Fill werkpostfiche'} className={styles["span-action-icons"]} onClick={() => handleActionClick('edit', eachRow)}><img src={edit_svg.src} alt="fill_werkpostfiche" className=''></img> </span>}
+          {eachRow.todo_status !== 1 && entityType !== 3 && <span title={'Fill werkpostfiche'} className={styles["span-action-icons"]} onClick={() => handleActionClick('edit', eachRow)}><img src={edit_svg.src} alt="fill_werkpostfiche" className=''></img> </span>}
           {eachRow.todo_status !== 1 && <span title={'Sign'} className={styles["span-action-icons"]} onClick={() => handleActionClick('sign', eachRow)}> <img src={sign_icon.src} alt="sign" className='sign_action_icon_size'></img> </span>}
         </> : eachRow.todo_status !== 1 && <span title={'Sign'} className={styles["span-action-icons"]} hidden={eachRow.todo_status === 1} onClick={() => handleActionClick('sign', eachRow)}> <img src={sign_icon_1.src} alt="sign" className=''></img> </span>
       }     {eachRow.todo_status === 1 && <span title={'Download'} className={styles["span-action-icons"]} onClick={() => handleActionClick('download', eachRow)}> <img src={download_svg.src} alt="download" className=''></img> </span>}
@@ -146,7 +146,7 @@ const TodosOverview = ({ props, entityId, entityType, tabId }) => {
       let { webform_id, submit_id, tid, employer_id = 0 } = eachRow;
       let path;
       if (type === 'edit')
-        path = `admin/structure/webform/manage/${webform_id}/submission/${submit_id}/edit?type=optout`;
+        path = `admin/structure/webform/manage/${webform_id}/submission/${submit_id}/edit?type=optout&rowId=${tid}`;
       if (type === 'sign')
         path = `werkpostfichespdf/form/werkpostfiche_preview/${webform_id}/${submit_id}/${tid}/${entityType === 3 ? employer_id : entityId}?type=${entityType === 2 ? 'employeer' : 'employee'}`
       if (type === 'download')
