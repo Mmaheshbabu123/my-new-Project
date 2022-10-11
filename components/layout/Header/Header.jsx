9de4e1c03241faee0902d24cadd292f2b84da68d
priya <logupriya.v@infanion.com>
2022-10-11 17:02:39 +0530

    design_fix

--- components/layout/Header/Header.jsx
+++ components/layout/Header/Header.jsx
@@ -11,7 +11,7 @@ function Header() {
 	// 	let dashboard_url = server_url.includes('test')?'https://test.absolute-you.infanion.com/dashboard?access=administrator&check_logged_in=1':
 	//    'http://uat.absolute-you.infanion.com/dashboard?access=administrator&check_logged_in=1';
 	return (
-		<div>
+		<div className='custom-position-sticky'>
 		<nav class="navbar navbar-expand-lg navbar-light bg-light d-sm-block d-md-none d-lg-none">
 	<div class="container px-0">
 
@@ -53,12 +53,12 @@ function Header() {
   </div>
 </nav>
 
-		<div className="custom-position-sticky ">
-			<div className="clip0 d-none d-md-block d-lg-block" />
+		<div className="custom-position-sticky d-sm-none d-md-block d-lg-block">
+			<div className="clip0" />
 			
-			<div className='container px-0'>
+			<div className='container px-0 custom-position-sticky'>
 				{/* <div className='row'> */}
-				<div className="custom-header border-bottom col-md-9 col-lg-11 m-auto border-2 pt-4 px-0">
+				<div className="custom-header border-bottom col-md-9 col-lg-11 m-auto border-2 custom-position-sticky px-0 pt-3">
 				{/* <nav className="navbar"> */}
 				{/* <div className = 'go_to_dashboard'>
 	<Link href={dashboard_url}>
