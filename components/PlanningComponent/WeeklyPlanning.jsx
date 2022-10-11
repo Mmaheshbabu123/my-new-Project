a589e324c64ec741f3ed1c6ad4267175fffd9534
supreetha <supreetha.s@infanion.com>
2022-10-11 11:24:03 +0530

    manage planning

--- components/PlanningComponent/WeeklyPlanning.jsx
+++ components/PlanningComponent/WeeklyPlanning.jsx
@@ -22,8 +22,7 @@ function WeeklyPlanning(props) {
 	const [ costcenterlist, setCostcenterlist ] = useState([]);
 	const [ edit, setEdit ] = useState(false);
 	const [ editDate, setEditDate ] = useState([]);
-	const [weekCount, setWeekCount] = useState(0);
-
+	const [ weekCount, setWeekCount ] = useState(0);
 
 	const [ styleEdit, setStyleEdit ] = useState('col-md-12');
 
@@ -39,7 +38,9 @@ function WeeklyPlanning(props) {
 		() => {
 			if (showview == true) {
 				if (company != '') {
-					APICALL.service(getWeeklyPlanning + company+'/'+weekCount, 'GET')
+					var loc = location != '' ? location : 0;
+					var cc = costcenter != '' ? costcenter : 0;
+					APICALL.service(getWeeklyPlanning + company + '/' + loc + '/' + cc + '/' + weekCount, 'GET')
 						.then((result) => {
 							if (result.status == 200) {
 								console.log(result.data[0]);
@@ -55,7 +56,7 @@ function WeeklyPlanning(props) {
 				}
 			}
 		},
-		[ showview, company, weekCount ]
+		[ showview, company, location, costcenter, weekCount ]
 	);
 
 	useEffect(
@@ -86,34 +87,66 @@ function WeeklyPlanning(props) {
 		window.scrollTo(0, 0);
 	};
 	let updateParent = () => {
-		
 		setStyleEdit('col-md-12');
 		setEditDate([]);
 		setEdit(false);
+	};
 
+	let updateLocation = (comp_id) => {
+		setCompany(comp_id);
+		var compObj = companylist.find((obj) => {
+			return obj.nid == comp_id ? obj : '';
+		});
 
-
-
-
-	}
+		let counter = 0;
+		locationlist.map((loc) => {
+			if (loc.comp_id == comp_id) counter++;
+		});
+		if (counter == 1) {
+			var result = locationlist.find((obj) => {
+				return obj.comp_id == comp_id ? obj : '';
+			});
+			if (result != '') {
+				setLocation(result.value);
+			}
+			// updateCostCenter(result.value);
+		} else {
+			setLocation('');
+		}
+	};
+	let updateCostCenter = (loc_id) => {
+		let counter = 0;
+		costcenterlist.map((loc) => {
+			if (loc.location_id == loc_id) counter++;
+		});
+		if (counter == 1) {
+			var result = costcenterlist.find((obj) => {
+				return obj.location_id == loc_id ? obj : '';
+			});
+			if (result != '') {
+				setCostcenter(result.value);
+			}
+		} else {
+			setCostcenter('');
+		}
+	};
 	return (
 		<div className="container-fluid p-0 m-0">
 			<div className="row">
-				<div className='row position-sticky-pc'>
-					<div className='col-md-12'>
-					<p className=" py-4 font-weight-bold   bitter-italic-normal-medium-24">Weekly planning</p>
-				{activeWeek &&
-				activeWeek.length > 0 && (
-					<p className=" poppins-light-18px pb-3">
-						For the week of Monday from {activeWeek[0].split('-').reverse().join('-')} to sunday{' '}
-						{activeWeek[6].split('-').reverse().join('-')}
-					</p>
-				)}
-
+				<div className="row position-sticky-pc">
+					<div className="col-md-12">
+						<p className=" py-4 font-weight-bold   bitter-italic-normal-medium-24">Weekly planning</p>
+						{activeWeek &&
+						activeWeek.length > 0 && (
+							<p className=" poppins-light-18px pb-3">
+								For the week of Monday from {activeWeek[0].split('-').reverse().join('-')} to sunday{' '}
+								{activeWeek[6].split('-').reverse().join('-')}
+							</p>
+						)}
 					</div>
 				</div>
 				<div className=" pt-3 d-flex justify-content-end planning_encodage_position">
-					<div className="d-inline ">
+					{/* <div className="d-inline ">
 						<button
 							type="button"
 							className="btn  btn my-2 skyblue-bg-color border-0 poppins-medium-18px  rounded-0 btn-block float-end mt-2 mb-2 d-flex align-items-center add-pln  px-3 btn-block shadow-none rounded-0 "
@@ -128,7 +161,7 @@ function WeeklyPlanning(props) {
 						>
 							Encodage view
 						</button>
-					</div>
+					</div> */}
 				</div>
 				{/* {showview && <p className="h6">For the week of Monday from 10/07/2022 to sunday 17/07/2022</p>}
 
@@ -147,45 +180,68 @@ function WeeklyPlanning(props) {
 					</div>
 				)} */}
 				<div className=" py-4 d-flex weekly_planning_search_position">
-			<div className='col-md-12'>
-			<div className='row'>
-					<div className='col-md-3'>
-					<select
-						className="form-select w-100 rounded-0 poppins-light-18px shadow-none rounded-0"
-						onChange={(e) => {
-							setCompany(e.target.value);
-						}}
-					>
-						<option value=""> Select company</option>
-						{companylist.map((value) => (
-							<option key={value.nid} value={value.nid}>
-								{value.title}
-							</option>
-						))}
-					</select>				
-					</div>
-					<div className='col-md-3'>
-					<select className="form-select w-100 poppins-light-18px shadow-none rounded-0">
-						<option value="">Select Location</option>
-						{locationlist.map((value) => (
+					<div className="col-md-12">
+						<div className="row">
+							<div className="col-md-3">
+								<select
+									value={company}
+									className="form-select w-100 rounded-0 poppins-light-18px shadow-none rounded-0"
+									onChange={(e) => {
+										updateLocation(e.target.value);
+									}}
+								>
+									<option value=""> Select company</option>
+									{companylist.map((value) => (
+										<option key={value.nid} value={value.nid}>
+											{value.title}
+										</option>
+									))}
+								</select>
+							</div>
+							<div className="col-md-3">
+								<select
+									className="form-select w-100 poppins-light-18px shadow-none rounded-0"
+									onChange={(e) => {
+										setLocation(e.target.value);
+										updateCostCenter(e.target.value);
+									}}
+									value={location}
+								>
+									<option value="">Select Location</option>
+									{company != '' &&
+										locationlist.map(
+											(options) =>
+												options.comp_id == company && (
+													<option key={options.value} value={options.value}>
+														{options.title}
+													</option>
+												)
+										)}
+									{/* {locationlist.map((value) => (
 							<option key={value.value} value={value.value}>
 								{value.title}
 							</option>
-						))}
-					</select>
+						))} */}
+								</select>
+							</div>
+							<div className="col-md-3">
+								<select
+									className="form-select w-100 poppins-light-18px shadow-none rounded-0"
+									value={costcenter}
+									onChange={(e) => {
+										setCostcenter(e.target.value);
+									}}
+								>
+									<option value="">Select cost center</option>
+									{costcenterlist.map((value) => (
+										<option key={value.value} value={value.value}>
+											{value.title}
+										</option>
+									))}
+								</select>
+							</div>
+						</div>
 					</div>
-					<div className='col-md-3'>
-					<select className="form-select w-100 poppins-light-18px shadow-none rounded-0">
-						<option value="">Cost center</option>
-						{costcenterlist.map((value) => (
-							<option key={value.value} value={value.value}>
-								{value.title}
-							</option>
-						))}
-					</select>
-					</div>
-				</div>
-			</div>
 					{/* <select className="form-select w-25 me-2">
 						<option>Select Project</option>
 						<option value="">Project-1</option>
@@ -193,12 +249,29 @@ function WeeklyPlanning(props) {
 					</select> */}
 				</div>
 				<div className={'mt-2 min-height-weekly-planning'}>
-					{(planning||company != '') ? (
+					{planning || company != '' ? (
 						<div className="row">
 							<div className={styleEdit}>
 								<p className={' bitter-italic-normal-medium-22 text-center table-title-bg py-3 '}>
-									<FaLessThan className="less-grather mx-4" onClick={()=>{setWeekCount(weekCount-1)}}/> <span onClick={()=>{setWeekCount(0)}}>Current week</span>{' '}
-									<FaGreaterThan className="less-grather mx-4" onClick={()=>{setWeekCount(weekCount+1)}}/>{' '}
+									<FaLessThan
+										className="less-grather mx-4"
+										onClick={() => {
+											setWeekCount(weekCount - 1);
+										}}
+									/>{' '}
+									<span
+										onClick={() => {
+											setWeekCount(0);
+										}}
+									>
+										Current week
+									</span>{' '}
+									<FaGreaterThan
+										className="less-grather mx-4"
+										onClick={() => {
+											setWeekCount(weekCount + 1);
+										}}
+									/>{' '}
 								</p>
 								<table className="table">
 									<thead className="">
@@ -245,55 +318,68 @@ function WeeklyPlanning(props) {
 										)}
 									</thead>
 									<tbody>
-										{Object.keys(planning).length>0?Object.keys(planning).map((value) => (
-											<tr
-												className="border-bottom table-border-gray equal-width-calc"
-												key={value}
-											>
-												{console.log(planning)}
-												{activeWeek.map((val, key) => (
-													<td className=" table-border-gray font-poppins-light" key={key}>
-														{planning[value].some((el) => el.pdate === val) ? (
+										{Object.keys(planning).length > 0 ? (
+											Object.keys(planning).map((value) => (
+												<tr
+													className="border-bottom table-border-gray equal-width-calc"
+													key={value}
+												>
+													{console.log(planning)}
+													{activeWeek.map((val, key) => (
+														<td className=" table-border-gray font-poppins-light" key={key}>
+															{planning[value].map(
+													(v2, k2) =>
+														v2.some((el) => el.pdate === val) ? (
 															<div>
-																{planning[value].map(
-																	(val1) =>
+																{v2.map(
+																	(val1, key1) =>
 																		val1.pdate == val ? (
 																			<div key={val1.id}>
-																				<div className='row mb-1'>
-																					<div className='col-md-9 pe-0'>
-																					<p className="color-skyblue pt-1">
-																					{val1.employee_name}
-																				</p>
+																				{key1 == 0 && (
+																					<div>
+																						<div className="row mb-1">
+																						<div className="col-md-9 pe-0">
+																							<p className="color-skyblue pt-1">
+																								{val1.employee_name}
+																							</p>
 																						</div>
 																						<div className="color-skyblue my-2 mt-1 text-end col-md-3">
-																					{new Date(val1.starttime) > new Date() ? <a>
-																						<MdEdit
-																							className="float-right"
-																							data-toggle="tooltip"
-																							title="Edit plannig"
-																							onClick={() =>
-																								editplanning(val1)}
-																						/>
-																					</a>:<span className='invisible'>edit</span>
-																					}
-																				</div>
+																							{new Date(val1.pdate) >
+																							new Date() ? (
+																								<a>
+																									<MdEdit
+																										className="float-right"
+																										data-toggle="tooltip"
+																										title="Edit plannig"
+																										onClick={() =>
+																											editplanning(
+																												val1
+																											)}
+																									/>
+																								</a>
+																							) : (
+																								<span className="invisible">
+																									edit
+																								</span>
+																							)}
+																						</div>
 																					</div>
-																				
-																				
-																				
-																				<p className="poppins-regular-16px mb-1">
-																					{val1.employee_type_name}
-																				</p>
-																				
-																				<p className="poppins-regular-16px mb-1">
-																					{val1.function_name}
-																				</p>
-																				
-																				<p className="poppins-regular-16px mb-1">
-																					{'€ ' + val1.salary}
-																				</p>
-																			
-																				<p className="poppins-regular-16px mb-1">
+																						<br />
+																						<p className="poppins-light-14px">
+																							{val1.employee_type_name}
+																						</p>
+																						<br />
+																						<p className="poppins-light-14px">
+																							{val1.function_name}
+																						</p>
+																						<br />
+																						<p className="poppins-light-14px">
+																							{'€ ' + val1.salary}
+																						</p>
+																						<br />
+																					</div>
+																				)}
+																				<p className="poppins-light-14px">
 																					{moment(val1.starttime).format(
 																						'HH:mm'
 																					) +
@@ -303,7 +389,7 @@ function WeeklyPlanning(props) {
 																						)}
 																				</p>
 
-																				
+																				<br />
 																			</div>
 																		) : (
 																			''
@@ -311,25 +397,41 @@ function WeeklyPlanning(props) {
 																)}
 															</div>
 														) : (
-															<div />
-														)}
-													</td>
-												))}
+															''
+														)
+												)}
+														</td>
+													))}
+												</tr>
+											))
+										) : company != '' ? (
+											<tr className="no-records">
+												<td
+													colSpan={7}
+													className="align-middle text-center poppins-light-18px border"
+													style={{ height: '4rem' }}
+												>
+													No planning for this week.
+												</td>
 											</tr>
-										)):company != ''?<tr className='no-records'>
-											<td colSpan={7} className="align-middle text-center poppins-light-18px border" style={{height:'4rem'}}>
-											No planning for this week.
-						</td></tr>:<tr>
-											<td colSpan={7} className="align-middle text-center poppins-light-18px border" style={{height:'4rem'}}>						
-							Select company and location to view planning.
-							</td></tr>}
+										) : (
+											<tr>
+												<td
+													colSpan={7}
+													className="align-middle text-center poppins-light-18px border"
+													style={{ height: '4rem' }}
+												>
+													Select company and location to view planning.
+												</td>
+											</tr>
+										)}
 									</tbody>
 								</table>
 							</div>
 
 							{edit && (
 								<div className="col-md-3">
-									<EditEmployee data={editDate} childtoparent={updateParent}/>
+									<EditEmployee data={editDate} childtoparent={updateParent} />
 								</div>
 							)}
 						</div>
@@ -342,7 +444,9 @@ function WeeklyPlanning(props) {
 				<div className="text-end mb-3">
 					<button type="submit" className="btn rounded-0  custom-btn px-3  btn-block float-end ">
 						<Link href={process.env.NEXT_PUBLIC_APP_URL_DRUPAL} className="">
-							<a className="btn rounded-0  custom-btn px-3  btn-block float-end poppins-medium-18px-next-button shadow-none">DASHBOARD</a>
+							<a className="btn rounded-0  custom-btn px-3  btn-block float-end poppins-medium-18px-next-button shadow-none">
+								DASHBOARD
+							</a>
 						</Link>
 					</button>
 				</div>
