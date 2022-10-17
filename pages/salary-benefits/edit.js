1c0e4926074458e6853c4907203671110f655d19
Sathish <sathish.kumar@infanion.com>
2022-10-17 19:26:40 +0530

    "chnages_salary_benefits_oct172022"

---
+++ pages/salary-benefits/edit.js
@@ -0,0 +1,9 @@
+import { useRouter } from 'next/router';
+import Salarybenfits from "../../components/PcComponent/AddSalaryBenefits";
+
+
+const edit = () => {
+	return <Salarybenfits/>;
+};
+
+export default edit;
