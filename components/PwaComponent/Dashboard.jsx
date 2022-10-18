4e69a64911097aa63adbcb14c08f7ed27fe96b0c
sreenu <sreenivasulu.perla@infanion.com>
2022-10-18 17:00:57 +0530

    changes in dashboard tile

--- components/PwaComponent/Dashboard.jsx
+++ components/PwaComponent/Dashboard.jsx
@@ -42,8 +42,8 @@ function Dashboard(props) {
     <div className="container">
       <p className="h3 px-0  bitter-italic-normal-medium-24 mt-2">Dashboard</p>
       <>
-	  {/* <DashboardTiles dashboardtiles = { dashboardtiles } /> */}
-          <p> Dashboard component </p> 
+	  <DashboardTiles dashboardtiles = { dashboardtiles } />
+          {/* <p> Dashboard component </p>  */}
 
       </>
       {/* <div>{dashboardtiles.length}</div> */}
