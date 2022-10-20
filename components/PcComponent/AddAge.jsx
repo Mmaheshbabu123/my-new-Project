103627ec343c9ee8645221d35874546cde9666fa
root <root@ip-172-31-25-155.eu-west-1.compute.internal>
2022-10-20 10:54:48 +0000

    issues

--- components/PcComponent/AddAge.jsx
+++ components/PcComponent/AddAge.jsx
@@ -1,4 +1,4 @@
-import { useState, useContext, useEffect } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import { PcContext } from '../../Contexts/PcContext';
 import ValidationService from '../../Services/ValidationService';
 import { addAge, getAge, updateAge } from '../../Services/ApiEndPoints';
