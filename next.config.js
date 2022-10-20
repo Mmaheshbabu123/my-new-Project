bde8c6f2752aea80a9372aee55deae3e2dfc2a80
sachin <sachinm.infanion@gmail.com>
2022-10-20 17:30:11 +0530

    code added related to config

--- next.config.js
+++ next.config.js
@@ -13,7 +13,11 @@ const nextConfig = {
   },
   cssLoaderOptions: {
     url: false
-  }
+  },
+  presets: [
+   "@babel/preset-env",
+    ["@babel/preset-react", {"runtime": "automatic"}]
+  ]
 }
 
 const withPWA = require('next-pwa')({
