d0d91d43f3c6f0b5a8336e0f682d7b8419eab01d
sachin <sachinm.infanion@gmail.com>
2022-10-20 11:31:32 +0530

    user logout page

---
+++ pages/user/logout.jsx
@@ -0,0 +1,19 @@
+import React, { useState, useEffect } from 'react';
+
+
+const UserLogout = () => {
+  const [ clear, setClear ] = useState(false);
+  useEffect(() => {
+    localStorage.clear();
+    setClear(!clear);
+  }, [])
+
+  if(clear) {
+    window.open('/user/login');
+  }
+
+return(
+  <>  </>
+)
+}
+export default UserLogout;
