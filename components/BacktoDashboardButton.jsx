fe6b0214fe1c06a42e77d3d001389654b4d33563
supreetha <supreetha.s@infanion.com>
2022-10-25 15:53:58 +0530

    back button

---
+++ components/BacktoDashboardButton.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { useRouter } from 'next/router';
+import Translation from '@/Translation';
+
+
+const BackToDashboardButton = (props) => {
+    const {t}=props;
+
+    const router = useRouter();
+	return (
+		<button type="button" style={{ background: 'none' }} className="btn  btn-block px-0 " onClick={() => {router.push('/')}}>
+			<span className="bg-white text-decoration-underline border-0 poppins-light-18px">{t('BACK')}</span>
+		</button>
+	);
+};
+
+
+export default React.memo(Translation(BackToDashboardButton,['BACK']));;
