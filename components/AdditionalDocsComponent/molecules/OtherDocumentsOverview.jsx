22e2687023b8cca5979aabaa9b6e193810d84814
sachin <sachinm.infanion@gmail.com>
2022-10-25 11:51:22 +0530

    code added related to labour todo changes and showing count on todo tile

---
+++ components/AdditionalDocsComponent/molecules/OtherDocumentsOverview.jsx
@@ -0,0 +1,265 @@
+import React, { useState, useEffect } from 'react';
+import { getOtherDocuments, downloadAdditionalDocuments } from '@/Services/ApiEndPoints';
+import { formatDate } from '../../SalaryBenefits/SalaryBenefitsHelpers';
+import ReactPaginate from 'react-paginate';
+import { APICALL } from '@/Services/ApiServices';
+import { GrView } from 'react-icons/gr';
+import { FiDownload } from 'react-icons/fi';
+import customAlert from '@/atoms/customAlert';
+import { getLastTwelveMonths } from '@/components/SalaryBenefits/SalaryBenefitsHelpers';
+const itemsPerPage = 8;
+
+const monthOptions = getLastTwelveMonths();
+const OtherDocumentsOverview = ({ entityId, entityType}) => {
+  const [state, setState] = useState({
+    loaded: false,
+    rows: [],
+    filterRows: [],
+    headers: ['Document', 'Employer', 'Company', 'Actions'],
+    employerSearchTerm: '',
+    companySearchTerm: '',
+    currentItems: [],
+    pageCount: 0,
+    itemOffset: 0,
+    currentPage: 0,
+  })
+
+  useEffect(() => {  fetchData() }, [])
+
+  /**
+   * [fetchData data fetching based on pcid]
+   * @return {Promise} [description]
+   */
+  const fetchData = async () => {
+    let setObj = {...state};
+    await APICALL.service(`${getOtherDocuments}/${entityId}/${entityType}`, 'GET').then(response => {
+      if(response.status === 200) {
+        let apiData = response.data;
+        setObj['rows'] = apiData;
+        setObj['filterRows'] = [...apiData || []];
+        setObj['loaded'] = true;
+      }
+    }).catch(error => console.error(error))
+    setState(setObj);
+  }
+
+
+  const handleSearchClick = (search = 0) => {
+    let { rows, employerSearchTerm, companySearchTerm } = state;
+    let filterRows = [];
+    if(search && (employerSearchTerm || companySearchTerm)) {
+      filterRows = rows.filter((item) => {
+        let status = true;
+        if(employerSearchTerm)
+          status = `${item['employer_name']}`.toLowerCase().toString().indexOf(employerSearchTerm.toLowerCase().toString()) !== -1;
+        if(status && companySearchTerm)
+          status = `${item['company_name']}`.toLowerCase().toString().indexOf(companySearchTerm.toLowerCase().toString()) !== -1;
+       return status;
+      })
+    } else {
+      filterRows = rows;
+      companySearchTerm = '';
+      employerSearchTerm = '';
+    }
+    setState({ ...state, companySearchTerm, employerSearchTerm,
+      filterRows: filterRows,
+      currentPage: 0,
+      itemOffset: 0,
+      ...updatePaginationData(filterRows, 0)
+    });
+  }
+
+  const getNeededActions = (eachRow) => {
+    return (
+      <>
+        <span title={'View'} className="actions-span text-dark" onClick={() => handleActionClick('preview', eachRow)}> <GrView /> </span>
+        <span title={'Download'} className="actions-span text-dark" onClick={() => handleActionClick('download', eachRow)}> <FiDownload/> </span>
+      </>
+    )
+  }
+
+  const handleActionClick = (action, eachRow) => {
+    if(action === 'preview') {
+      window.open(`/v1-document?entityid=${eachRow.employee_id}&entitytype=${3}&companyid=${eachRow.company_id}&preview=${1}`, '_blank');
+    }
+    if(action === 'download') {
+      handleDownload(eachRow);
+    }
+  }
+
+  const handleDownload = async (eachRow) => {
+    const {fid, file_path, file_location, file_name } = eachRow;
+    eachRow['files'] = [{ fid, file_path, file_location, file_name }];
+    setState({...state, spinner: true})
+    await APICALL.service(`${downloadAdditionalDocuments}`, 'POST', eachRow)
+      .then((response) => {
+        let result = response.data;
+        if(response.status === 200 && result.url) {
+          setState({...state, spinner: false, showPopup: false})
+          var a = document.createElement("a");
+          a.setAttribute("type", "file");
+          a.href     = result.url;
+          a.download = result.zipFileName;
+          a.target   = '_blank';
+          document.body.appendChild(a);
+          a.click();
+          a.remove();
+        } else {
+          setState({...state, spinner: false, showPopup: false})
+          customAlert('error', `Error occurred while downloading`, 2000);
+        }
+    }).catch((error) => {
+      customAlert('error', `Error occurred while downloading`, 2000);
+      setState({...state, spinner: false, showPopup: false})
+    })
+  }
+
+  //------------------- Pagination code -------------------------//
+  //-------------------
+     useEffect(() => {
+       if(state.loaded) {
+         setState({...state, ...updatePaginationData(state.filterRows, state.itemOffset || 0)})
+       }
+     }, [state.itemOffset, state.loaded]);
+
+     const updatePaginationData = (filterRows, offset) => {
+       let items = [...filterRows];
+       const endOffset = offset + itemsPerPage;
+       return {
+         currentItems: items.slice(offset, endOffset),
+         pageCount: Math.ceil(items.length / itemsPerPage)
+       };
+     }
+
+     const handlePageClick = (event) => {
+       let items = [...state.filterRows];
+       const newOffset = (event.selected * itemsPerPage) % items.length;
+       setState({...state, itemOffset: newOffset, currentPage: event.selected});
+     };
+  //------------------- Pagination code -------------------------//
+  //-------------------
+
+
+  if(!state.loaded) {
+    return <> Loading... </>
+  }
+
+  const searchTextField = (key, placeholder) => {
+    return(
+        <div className='col-md-3' >
+        <input
+          type="text"
+          value={state[key]}
+          className="form-control mt-2 mb-2 input-border-lightgray poppins-regular-18px mh-50 rounded-0 shadow-none"
+          onChange={(e) => setState({...state, [key]: e.target.value })}
+          placeholder={'Search ' + placeholder}
+          onKeyUp={(e) => e.key === 'Enter' ? handleSearchClick(1): null}
+        />
+
+      </div>
+    )
+  }
+console.log(state);
+
+  return(
+    <div>
+    <div className='searchbox m-0 v1_doc v1_position_sticky' style={{ margin: '10px 0', position: 'relative' }}>
+    <div className='row '>
+    <div className='col-md-12'>
+       <div className='row'>
+       <div className='col-md-9'>
+         <div className='row'>
+         {searchTextField('employerSearchTerm', 'employee')}
+         {searchTextField('companySearchTerm', 'company')}
+         </div>
+       </div>
+       <div className='col-md-3'>
+         <div className='row'>
+           <div className='col-md-6'>
+           <button
+           type="button"
+           className="btn  btn-block border-0 rounded-0 float-right mt-2 mb-2 ms-2 skyblue-bg-color shadow-none w-100"
+           onClick={() => handleSearchClick(1)}
+         >
+           SEARCH
+         </button>
+
+           </div>
+           <div className='col-md-6'>
+           <button
+           type="button"
+           className="btn border-0 btn-block rounded-0 float-right mt-2 mb-2 reset_skyblue_button shadow-none w-100"
+           onClick={() => handleSearchClick(0)}
+         >
+           RESET
+         </button>
+           </div>
+         </div>
+       </div>
+       </div>
+      </div>
+    </div>
+    </div>
+    <div className="table-render-parent-div minimun_height_v1">
+        <table className="table table-hover manage-types-table manage-documents-table-header ">
+          <thead className="table-render-thead">
+            <tr key={'header-row-tr'}>{state.headers.map((eachHeader, index) => <th key={`tablecol${index}`} className="align-middle" scope="col"> {eachHeader} </th>)} </tr>
+          </thead>
+          {state.currentItems && state.currentItems.length > 0 ?
+          <tbody>
+            {state.currentItems.map(eachRow => <tr key={eachRow.tid} id={eachRow.tid}>
+              <td> {eachRow.title}  </td>
+              <td> {eachRow.employer_name} </td>
+              <td> {eachRow.company_name} </td>
+              <td>{ getNeededActions(eachRow) } </td>
+            </tr>)}
+          </tbody>
+          : <tbody>
+          <tr>
+          <td colSpan={8} className="text-center poppins-regular-18px no-records">
+                  No records
+                </td>
+          </tr>
+          </tbody>}
+        </table>
+    </div>
+    <div>
+    {state.filterRows.length > itemsPerPage && <ReactPaginate
+        breakLabel="..."
+        nextLabel="Next >"
+        onPageChange={handlePageClick}
+        pageRangeDisplayed={5}
+        pageCount={state.pageCount}
+        forcePage={state.currentPage}
+        previousLabel="< Previous"
+        renderOnZeroPageCount={null}
+        containerClassName={"pagination"}
+        itemClass="page-item"
+        linkClass="page-link"
+        subContainerClassName={"pages pagination"}
+        activeClassName={"active"}
+    />}
+     <div className='row justify-content-end'>
+     <div className="col-md-1">
+        <button
+          type="button"
+          className="btn  btn-block border-0 rounded-0 float-right mt-2 mb-2 skyblue-bg-color w-100 shadow-none"
+          onClick={() => setState({...state, showPopup: true})}
+        >
+          Export
+        </button>
+    </div>
+     </div>
+     <div className='row'>
+       <div className='col-md-12'>
+       <button onClick={() => window.open(process.env.NEXT_PUBLIC_APP_URL_DRUPAL, '_self')} type="button" className="btn text-decoration-underline text-uppercase poppins-light-18px shadow-none px-0">
+        {`Back`}
+      </button>
+       </div>
+     </div>
+    </div>
+    </div>
+  );
+}
+
+export default React.memo(OtherDocumentsOverview)
