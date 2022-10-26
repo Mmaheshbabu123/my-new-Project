721ce1201c4005b2ebc6e7be64c1b57bdc24aa14
supreetha <supreetha.s@infanion.com>
2022-10-26 20:00:17 +0530

    add pc

--- pages/add-pc.js
+++ pages/add-pc.js
@@ -1,12 +1,10 @@
 import React, { useEffect } from 'react';
 import Router from 'next/router';
 import { Uniquekey } from '../Services/GenetateUniqueKey';
-import { useRouter } from 'next/router';
 
 
 
 const addpc = () => {
-    const router = useRouter();
     const unique_key = Uniquekey.generate_unique_key();
     // const history = useHistory();
     // const { pathname } = Router;
