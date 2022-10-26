5d07d78948587a9fe126000e9dcd76395ad95d0f
Sathish <sathish.kumar@infanion.com>
2022-10-26 17:45:54 +0530

    "chnages"

--- pages/timeregistration/index.js
+++ pages/timeregistration/index.js
@@ -1,11 +1,11 @@
 import { useRouter } from 'next/router';
 import Device from '@/components/TimeRegistration/CheckDevice';
 
-const check = () => {
+const Check = () => {
     const router = useRouter();
     return (
         <div><Device /></div>
     );
 }
 
-export default check;
+export default Check;
