c91e08ff2a334081e8f59acbb043bdbd8caa8019
Sathish <sathish.kumar@infanion.com>
2022-11-09 16:29:18 +0530

    encodage_nov9_2022

--- pages/timeregistration/qrcode.js
+++ pages/timeregistration/qrcode.js
@@ -1,16 +1,22 @@
 import { useRouter } from 'next/router';
+import {useContext} from 'react';
 import Image from "next/image";
+import UserAuthContext from '@/Contexts/UserContext/UserAuthContext';
+import React from 'react';
 
 const Qrcode = (props) => {
+
+const { contextState = {} } = useContext(UserAuthContext);
     const { key, t } = props;
     const router = useRouter();
 
     const redirectTab=()=>{
         router.push('/time-registration')
     }
+    var res=<h1>...Loading..........</h1>;
 
-    return (
-      <div className='row'>                    
+    // (contextState.role='employee')?
+     res=<div className='row'>                    
       
             <div className='col-md-12 text-center d-flex align-items-center justify-content-center pt-3 qr-code-time-registration'>
                         <Image src="/images/qrcodecommon.svg"  className="dasboard_image"
@@ -32,8 +38,11 @@ const Qrcode = (props) => {
                      </div>
                    </div>
     </div>
+    // res=<div>
 
-    );
+    // </div>;
+
+return res;
 }
 
 export default Qrcode;
