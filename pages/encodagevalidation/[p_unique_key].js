5e5b8e7ad0718dc77cf08b91a885593613a50510
Sathish <sathish.kumar@infanion.com>
2022-11-22 13:33:04 +0530

    enocadage_nov22_2022

--- pages/encodagevalidation/[p_unique_key].js
+++ pages/encodagevalidation/[p_unique_key].js
@@ -9,16 +9,16 @@ const Encodage = () => {
   return (
     <div>
     <nav>
-    <div class="nav nav-tabs" id="nav-tab" role="tablist">
-      <button class="nav-link active" id="nav-home-tab" data-bs-toggle="tab" data-bs-target="#nav-home" type="button" role="tab" aria-controls="nav-home" aria-selected="true" onClick={()=>setActive(false)}>Unvalidated</button>
-      <button class="nav-link" id="nav-profile-tab" data-bs-toggle="tab" data-bs-target="#nav-profile" type="button" role="tab" aria-controls="nav-profile" aria-selected="false" onClick={()=>setActive(true)}>Validated</button>
+    <div className="nav nav-tabs" id="nav-tab" role="tablist">
+      <button className="nav-link active" id="nav-home-tab" data-bs-toggle="tab" data-bs-target="#nav-home" type="button" role="tab" aria-controls="nav-home" aria-selected="true" onClick={()=>setActive(false)}>Unvalidated</button>
+      <button className="nav-link" id="nav-profile-tab" data-bs-toggle="tab" data-bs-target="#nav-profile" type="button" role="tab" aria-controls="nav-profile" aria-selected="false" onClick={()=>setActive(true)}>Validated</button>
     </div>
   </nav>
-  <div class="tab-content" id="nav-tabContent">
-    <div class="tab-pane fade show active" id="nav-home" active="true" role="tabpanel" aria-labelledby="nav-home-tab">
+  <div className="tab-content" id="nav-tabContent">
+    <div className="tab-pane fade show active" id="nav-home" active="true" role="tabpanel" aria-labelledby="nav-home-tab">
       <Encdage/>
     </div>
-    <div class="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab">
+    <div className="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab">
     <Validatedencodage/>
     </div>
   </div>
